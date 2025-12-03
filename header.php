75c2280a371891b8faba5ce74aaf57b2470ac728
adaxoooo-source <adaxoooo@icloud.com>
2025-12-03 14:46:19 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,9 @@
+<header class="topbar">
+    <h2>Restaurant Élégant</h2>
+    <nav>
+        <a href="index.php">Accueil</a>
+        <a href="menu.php">Menu</a>
+        <a href="reservations.php">Réservations</a>
+        <a href="contact.php">Contact</a>
+    </nav>
+</header>
